12ee4778ceeaf1e106fc2bab229848fb845cd3df
Philipp Frenzel <philipp@frenzel.net>
2014-04-19 21:08:51 +0200

    adding plain migrations...

---
+++ migrations/m140419_190638_handycap.php
@@ -0,0 +1,18 @@
+<?php
+
+use yii\db\Schema;
+
+class m140419_190638_handycap extends \yii\db\Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m140419_190638_handycap cannot be reverted.\n";
+
+        return false;
+    }
+}
---
+++ migrations/m140419_190823_golferattendance.php
@@ -0,0 +1,18 @@
+<?php
+
+use yii\db\Schema;
+
+class m140419_190823_golferattendance extends \yii\db\Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m140419_190823_golferattendance cannot be reverted.\n";
+
+        return false;
+    }
+}
