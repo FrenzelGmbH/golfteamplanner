644ab0502136232fda74be1dccb4d72b82bfbebb
Philipp Frenzel <philipp@frenzel.net>
2014-04-20 09:22:15 +0200

    add needed migrations

--- migrations/m140419_190638_handycap.php
+++ migrations/m140419_190638_handycap.php
@@ -6,13 +6,18 @@ class m140419_190638_handycap extends \yii\db\Migration
 {
     public function up()
     {
-
+    	$this->createTable('{{%handycap}}',array(
+          'id'                      => Schema::TYPE_PK,
+          'user_id'                 => Schema::TYPE_INTEGER.' DEFAULT NULL',
+          'hcp'                     => Schema::TYPE_FLOAT.' DEFAULT 36.0',          
+          'status'                  => Schema::TYPE_STRING .'(255) NOT NULL DEFAULT "created"',
+          'time_deleted'            => Schema::TYPE_INTEGER.' DEFAULT NULL',
+          'time_create'             => Schema::TYPE_INTEGER,
+      ),'CHARACTER SET utf8 COLLATE utf8_bin ENGINE = InnoDB;');
     }
 
     public function down()
     {
-        echo "m140419_190638_handycap cannot be reverted.\n";
-
-        return false;
+        $this->dropTable('{{%handycap}}');
     }
 }
--- migrations/m140419_190729_teamevent.php
+++ migrations/m140419_190729_teamevent.php
@@ -6,13 +6,21 @@ class m140419_190729_teamevent extends \yii\db\Migration
 {
     public function up()
     {
-
+    	$this->createTable('{{%teamevent}}',array(
+          'id'                      => Schema::TYPE_PK,
+          'name'                    => Schema::TYPE_STRING .'(255) NOT NULL DEFAULT "Teamplay"',
+          'time_start'				=> 'TIME',
+		  'time_end'				=> 'TIME',
+		  'date_start'				=> 'DATE',
+		  'date_end'				=> 'DATE',
+          'status'                  => Schema::TYPE_STRING .'(255) NOT NULL DEFAULT "created"',
+          'time_deleted'            => Schema::TYPE_INTEGER.' DEFAULT NULL',
+          'time_create'             => Schema::TYPE_INTEGER,
+      	),'CHARACTER SET utf8 COLLATE utf8_bin ENGINE = InnoDB;');
     }
 
     public function down()
     {
-        echo "m140419_190729_teamevent cannot be reverted.\n";
-
-        return false;
+        $this->dropTable('{{%teamevent}}');
     }
 }
